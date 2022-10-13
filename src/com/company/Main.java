c1df732bcc91dfd115f2b2b4e1d0347f66b4ca07
LauraLSJN <lsjn@ruc.dk>
2022-10-13 12:01:29 +0200

    ...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,35 @@ public class Main {
      //   MatrixGraph directedG=createDirected();
        // directedG.PrintGraph();
 
-        AdjacencyGraph adjDirectedG=createDirectedAdj();
+        AdjacencyGraph adjDirectedG = createDirectedAdj();
         adjDirectedG.PrintGraph();
 
+               //D... (Shortest Path) Algoritme
+               int n = adjDirectedG.Vertices.size();
+               ArrayList<Vertex> vlist = adjDirectedG.Vertices;
+               boolean[] done = new boolean[n];
+               Vertex[] prev = new Vertex[n];
+               int [] weight = new int[n];
+
+               Arrays.fill(weight,100000);
+               Arrays.fill(done,false);
+               weight[0] = 0;
+               prev[0] = adjDirectedG.Vertices.get(0);
+
+               Vertex current = null;
+               int vægt = 100000;
+            for (int i = 0; i <n ; i++) {
+             if (weight[i] < vægt &&! done[i] ){
+                 current = adjDirectedG.Vertices.get(i);
+                 vægt = weight[i];
+             }
+                //System.out.println("current: " + current);
+            }
+        System.out.println("current: " + current);
+
+        for (int i = 0; i <n ; i++) {
+            System.out.println(i + "  " + done[i] + " " + prev[i] + " " + weight[i]);
+        }
     }
 
 
