527589446c80890a33d4954937f64b52ccc7c69e
LauraLSJN <lsjn@ruc.dk>
2022-10-13 11:44:56 +0200

    ...

---
+++ src/com/company/AdjacencyGraph.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class AdjacencyGraph {
+    ArrayList<Vertex> Vertices;
+    public AdjacencyGraph(){
+
+        Vertices=new ArrayList<Vertex>();
+    }
+
+    public void addVertex(Vertex v){
+        Vertices.add(v);
+    }
+
+    public void addEdge(Vertex from, Vertex to, Integer container){
+        if(!(Vertices.contains(from) && Vertices.contains(to)))
+        {
+            System.out.println("Vertices missing from graph");
+            return;
+        }
+        Edge newE = new Edge(from, to, container);
+
+        for (int i = 0; i <Vertices.size(); i++) {
+             if (from == Vertices.get(i)){
+                 System.out.println(Vertices.get(i).name + ": " + container );
+             }
+        }
+
+    }
+
+
+    public void addUnDirectedEdge(Vertex from, Vertex to, Integer weight){
+        if(!(Vertices.contains(from) && Vertices.contains(to)))
+        {
+            System.out.println("Vertices missing from graph");
+            return;
+        }
+        Edge newE=new Edge(from, to, weight);
+        Edge newE2=new Edge( to, from, weight);
+    }
+
+    public void PrintGraph(){
+        for(int i=0;i<Vertices.size();i++){
+            System.out.println(" Destination "+Vertices.get(i).name+" is shipping container amount to: ");
+            Vertex current=Vertices.get(i);
+            for (Edge e: current.OutEdge) {
+                System.out.println(e.to.name +" with container amount: "+e.weight);
+            }
+        }
+
+    }
+
+}
+
+class Vertex {
+    String name;
+    ArrayList<Edge> OutEdge;
+    public Vertex(String name){
+        this.name=name;
+        OutEdge=new ArrayList<Edge>();
+    }
+
+}
+
+class Edge{
+    Vertex from;
+    Vertex to;
+    Integer weight;
+    public Edge(Vertex from,Vertex to, Integer weight){
+        this.from=from;
+        this.to=to;
+        this.weight=weight;
+        from.OutEdge.add(this);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,65 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+     //   MatrixGraph directedG=createDirected();
+       // directedG.PrintGraph();
+
+        AdjacencyGraph adjDirectedG=createDirectedAdj();
+        adjDirectedG.PrintGraph();
+
     }
+
+
+
+    public static AdjacencyGraph createDirectedAdj(){
+        AdjacencyGraph newG=new AdjacencyGraph();
+
+        //Laver en ny Vertex med navn
+        Vertex Jaw =new Vertex("Jaw");
+        Vertex Tan =new Vertex("Tan");
+        Vertex Dar =new Vertex("Dar");
+        Vertex Mom =new Vertex("Mom");
+        Vertex Jeb =new Vertex("Jeb");
+        Vertex Zan =new Vertex("Zan");
+        Vertex Sal =new Vertex("Sal");
+
+        //tilføjer Vertex til arrayliste
+        newG.addVertex(Jaw);
+        newG.addVertex(Tan);
+        newG.addVertex(Dar);
+        newG.addVertex(Mom);
+        newG.addVertex(Jeb);
+        newG.addVertex(Zan);
+        newG.addVertex(Sal);
+
+        //Fra en destination til en anden -> hvor mange containers der bliver flyttet
+        //Fra havn Jaw
+        newG.addEdge(Jaw,Dar,2000);
+        newG.addEdge(Jaw,Mom,2000);
+
+        //Fra havn Tan
+        newG.addEdge(Tan,Mom,5000);
+        newG.addEdge(Tan,Jeb,7000);
+        newG.addEdge(Tan,Dar,3000);
+        newG.addEdge(Tan,Zan,2000);
+        newG.addEdge(Tan,Sal,7000);
+
+        //Fra havn Dar
+        newG.addEdge(Dar,Jaw,3000);
+        newG.addEdge(Dar,Jeb,2000);
+        newG.addEdge(Dar,Tan,5000);
+
+        //Fra havn Mom
+        newG.addEdge(Mom,Jeb,500);
+        newG.addEdge(Mom,Sal, 2000);
+
+
+        newG.addEdge(Jeb,Jeb,0);
+        newG.addEdge(Zan,Zan,0);
+        newG.addEdge(Sal,Sal,0);
+
+        return newG;
+    }
+
+    
 }
